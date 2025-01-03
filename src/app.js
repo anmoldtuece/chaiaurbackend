20d711d33efacbcc2b5ed859b0bab4c4cffbeb5e
anmoldtuece <155827861+anmoldtuece@users.noreply.github.com>
2025-01-03 14:36:47 +0530

    add user registration route and controller; update app configuration

--- src/app.js
+++ src/app.js
@@ -17,7 +17,14 @@ app.use((req, res, next) => {
     next();
 })
 
+//routes
+
+import userRouter from './routes/user.routes.js';
 
 
+//routes declaration
+app.use('/api/v1/users', userRouter);
+
+//http://localhost:8000/api/v1/users/register
 
 export default app;
